1e2e029ecf3356f6dc22616fbf69cd5870c78416
yunxiyi <yunxiyi@outlook.com>
2018-07-23 00:07:30 +0800

    remove no used import

--- dubbo-registry/dubbo-registry-eureka/src/main/java/org/apache/dubbo/registry/eureka/EurekaRegistry.java
+++ dubbo-registry/dubbo-registry-eureka/src/main/java/org/apache/dubbo/registry/eureka/EurekaRegistry.java
@@ -34,7 +34,6 @@ import org.apache.dubbo.registry.support.FailbackRegistry;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
